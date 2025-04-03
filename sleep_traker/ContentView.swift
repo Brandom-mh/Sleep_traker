fb4a67d5736677128b0a0a3e05afe9a6f041380d
Alumno <alumno@MacBook-Air-22.local>
2025-04-03 11:03:30 -0600

    first commit

---
+++ sleep_traker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sleep_traker
+//
+//  Created by Alumno on 03/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Hora de acostares"){
+                print("button action")
+            }.frame(width: 200, height: 150).background(.colo)
+            Image("clock")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sleep_traker/sleep_trakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleep_trakerApp.swift
+//  sleep_traker
+//
+//  Created by Alumno on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct sleep_trakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
